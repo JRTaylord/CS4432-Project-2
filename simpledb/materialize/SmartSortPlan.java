258397d5e7bd291ccf02b9430702cbeb7d8c7310
James Taylor <james.taylor2@comcast.net>
2018-04-25 20:54:32 -0400

    Reworked more shit
    Also added to the README

--- simpledb/materialize/SmartSortPlan.java
+++ simpledb/materialize/SmartSortPlan.java
@@ -1,5 +1,6 @@
 package simpledb.materialize;
 
+import simpledb.metadata.MetadataMgr;
 import simpledb.query.Plan;
 import simpledb.query.Scan;
 import simpledb.query.TableScan;
@@ -11,9 +12,11 @@ import java.util.List;
  * Project 2
  */
 public class SmartSortPlan extends SortPlan{
+    private String tableName;
 
-    public SmartSortPlan(Plan p, List<String> sortfields, Transaction tx){
+    public SmartSortPlan(Plan p, List<String> sortfields, Transaction tx, String tableName){
         super(p, sortfields, tx);
+        this.tableName = tableName;
     }
 
     /**
@@ -22,16 +25,21 @@ public class SmartSortPlan extends SortPlan{
      */
     @Override
     public Scan open(){
+
+        //Project 2: finds if the table is already sort through the MetadataMgr
+        boolean sorted = false;
+        MetadataMgr mdm = new MetadataMgr(false, tx);
+        sorted = mdm.getTableInfo(tableName, tx).getSorted();
+
+        if(sorted){
+            return new TableScan(mdm.getTableInfo(tableName, tx), tx);
+        }
+
         Scan src = p.open();
         List<TempTable> runs = splitIntoRuns(src);
         src.close();
         while (runs.size() > 2)
             runs = doAMergeIteration(runs);
-        if(runs.size()==1){
-            if(runs.get(0).getTableInfo().getSorted()){
-                return new TableScan(runs.get(0).getTableInfo(), tx);
-            }
-        }
         return new SmartSortScan(runs, comp);
     }
 }
